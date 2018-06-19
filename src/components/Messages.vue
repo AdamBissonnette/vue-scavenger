aa7bfbecf202b8da985d91f63324cf4d143096f6
Adam <adam@mediamanifesto.com>
2018-06-19 10:37:49 -0600

    Playing around with mock api

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -2,26 +2,55 @@
   <div class="messages">
     {{msg}}
     <ul>
-      <li v-for="result in messages">
-        {{result.title}} - {{result.abstract}}
+      <li v-for="message in sortedMessages">
+        {{message.text}}, {{message.sent}}
       </li>
     </ul>
   </div>
 </template>
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script>
 export default {
   name: 'Messages',
   data () {
     return {
       msg: "I feel like I get this - here are the messages:",
-      messages: [{title: "New Message", abstract: "Rwar"}]
+      messages: [{
+            "uid": "db5c08222ba14539b509a3a772512a92",
+            "text": "Earlier",
+            "group_uid": "E21293",
+            "story_uid": "IOTLANG",
+            "receiver": "messenger:2075608205815078",
+            "media_url": null,
+            "sent": "2018-06-07T00:28:58.677190",
+            "sender": null}, 
+            {"uid": "ab5c08222ba14539b509a3a772512a92",
+            "text": "Later",
+            "group_uid": "E21293",
+            "story_uid": "IOTLANG",
+            "receiver": "messenger:2075608205815078",
+            "media_url": null,
+            "sent": "2018-07-07T00:28:58.677190",
+            "sender": null}]
     }
   },
-  mounted() {
-    axios.get('https://ade1dd2f-be70-4975-9776-49456a772db0.mock.pstmn.io/messagest')
-      .then(response => {this.messages = response.data.results})
+  computed: {
+    sortedMessages: function() {
+      function compare(a, b) {
+        return new Date(a.sent) - new Date(b.sent);
+      }
+      return this.messages.sort(compare);
+    }
   }
+  // mounted() {
+  //   axios.get('https://4e42a181-6440-4b26-a848-39b7f9481286.mock.pstmn.io/messages')
+  //     .then(response => {
+  //       function compare(a, b) {
+  //         return new Date(a.sent) - new Date(b.sent);
+  //       }
+
+  //       this.messages = response.data.data;
+  //     })
+  // }
 }
 </script>
 
