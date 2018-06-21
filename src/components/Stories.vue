ebc0aff56d3cf87a40c9edd357617c06c17a7a49
Adam <adam@mediamanifesto.com>
2018-06-21 11:07:38 -0600

    I don't know what I just did...

--- src/components/Stories.vue
+++ src/components/Stories.vue
@@ -5,13 +5,19 @@
     <input type="text" id="addStoryName" v-model="storyName" />
     <button @click="addStory">Add</button>
 
-    <div class="story" v-for="story in stories">
+    <div class="story" v-for="(story, index) in stories">
       <h3>{{story.uid}}</h3>
+
       <label :for="story.uid + '_end_message'">End Message</label>
       <textarea :id="story.uid + '_end_message'" v-model="story.end_message"></textarea>
       <label :for="story.uid + '_default_hint'">Default Hint</label>
       <textarea :id="story.uid + '_default_hint'" v-model="story.default_hint"></textarea>
       
+      <div class="controls">
+        <button @click="saveStory(index)">Save</button>
+        <button @click="delStory(index)">Delete</button>
+      </div>
+
     </div>
   </div>
 </template>
@@ -23,47 +29,52 @@ export default {
   data () {
     return {
       storyName: '',
-      stories: [{
-            "clues": [
-                "TEST:START",
-                "TEST:CLUE1",
-                "TEST:CLUE2",
-                "TEST:CLUE3",
-                "TEST:CLUE4"
-            ],
-            "uid": "TEST",
-            "default_hint": "test",
-            "end_message": "Looks like you've hit the end of the story, text 'restart' to try again!"
-        },
-        {
-            "clues": [
-                "TESTA:START",
-                "TESTA:CLUE1",
-                "TESTA:CLUE2",
-                "TESTA:END"
-            ],
-            "uid": "TESTA",
-            "default_hint": "test",
-            "end_message": "Looks like you've hit the end of the story, text 'restart' to try again!"
-        }]
+      stories: []
     }
+  },
+  mounted() {
+    axios.get('/api/stories/')
+      .then(response => {
+        this.stories = response.data.data;
+      })
   },
   methods: {
     addStory: function() {
-      axios.put(`http://jsonplaceholder.typicode.com/posts`, {
-        body: this.postBody
+      axios.put(`/api/stories/` + this.storyName.toUpperCase(), {
+        uid: this.storyName.toUpperCase(),
+        default_hint:""
       })
-      .then(response => {})
+      .then(response => {
+        this.stories.push(response.data.data)
+      })
       .catch(e => {
         this.errors.push(e)
       })
-      this.stories.push({
-            "clues": [],
-            "uid": this.storyName,
-            "default_hint": "",
-            "end_message": ""
-        })
       this.storyName = ""
+    },
+    saveStory: function(index) {
+      axios.get(`/api/stories/` + this.stories[index].uid)
+      .then(response => {
+        var story = response.data.data
+        story.default_hint = this.stories[index].default_hint
+        story.end_message = this.stories[index].end_message
+        this.stories[index] = story
+
+        axios.put(`/api/stories/` + this.stories[index].uid, {
+          uid: this.stories[index].uid,
+          default_hint: this.stories[index].default_hint,
+          end_message: this.stories[index].end_message,
+        })
+        .then(response => {
+          this.stories[index] = response.data.data
+        })
+      })
+    },
+    delStory: function(index) {
+      axios.delete(`/api/stories/` + this.stories[index].uid)
+      .then(response => {
+        this.stories.splice(index, 1)
+      })
     }
   },
   components: {
